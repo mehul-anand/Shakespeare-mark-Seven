79240be79e1dd4b29e166fe3f3f7a79f5f6dc3bc
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-14 04:35:45 +0530

    generate url

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ var txtInput = document.querySelector("#text-input")
 var outputDiv = document.querySelector("#output")
 
 var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
+
+function getTranslationURL(input) {
+    var text = serverURL + "?" + "text=" + input
+    console.log("text in fn=" + text)
+    return text
+}
