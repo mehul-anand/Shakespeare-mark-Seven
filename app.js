c4c73161c855457cc84945e76b17f6d841c409a9
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-14 04:35:11 +0530

    till url

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#text-input")
+var outputDiv = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
